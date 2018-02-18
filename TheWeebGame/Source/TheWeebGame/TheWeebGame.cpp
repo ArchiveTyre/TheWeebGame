d437b0b2878256eca081ab85a84d0bf1d470fdd9
Alexander Björkman <ab3.git@eit.se>
2018-02-18 14:44:55 +0100

    Switcehd to UnrealEngine

---
+++ TheWeebGame/Source/TheWeebGame/TheWeebGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheWeebGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheWeebGame, "TheWeebGame" );
